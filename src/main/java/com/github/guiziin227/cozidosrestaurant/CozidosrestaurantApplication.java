0c2b7aec7eccddebfeecc7e0dd93bc2cdead81bd
Guiziin227 <gh63388@gmail.com>
2025-08-06 16:16:35 -0300

    first commit

---
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/CozidosrestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.github.guiziin227.cozidosrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozidosrestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CozidosrestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/guiziin227/cozidosrestaurant/CozidosrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guiziin227.cozidosrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozidosrestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
