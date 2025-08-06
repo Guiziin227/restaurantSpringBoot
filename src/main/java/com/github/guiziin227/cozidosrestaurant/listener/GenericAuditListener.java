30a7146437fe6b0b29db504f88078fd1de768867
Guiziin227 <gh63388@gmail.com>
2025-08-06 18:43:42 -0300

    GenericAuditListener para createAt e updatedAt

---
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/listener/GenericAuditListener.java
@@ -0,0 +1,30 @@
+package com.github.guiziin227.cozidosrestaurant.listener;
+
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class GenericAuditListener {
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+    @PrePersist
+    public void prePersist(Object entity) {
+        if (entity instanceof Auditable auditable) {
+            var now = java.time.LocalDateTime.now();
+            auditable.setCreatedAt(now);
+            auditable.setUpdatedAt(now);
+            System.out.println("Entity created at: " + now.format(formatter));
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate(Object entity) {
+        if (entity instanceof Auditable auditable) {
+            var now = java.time.LocalDateTime.now();
+            auditable.setUpdatedAt(now);
+        }
+    }
+}
