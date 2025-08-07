39ebf48a3eb6ca46431e146d0da63a9f9cfae82c
Guiziin227 <gh63388@gmail.com>
2025-08-07 11:06:32 -0300

    Refactor ClientController to use DTOs for request and response; integrate ClientMapper for entity conversion

--- src/main/java/com/github/guiziin227/cozidosrestaurant/controller/ClientController.java
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/controller/ClientController.java
@@ -1,7 +1,11 @@
 package com.github.guiziin227.cozidosrestaurant.controller;
 
+import com.github.guiziin227.cozidosrestaurant.dto.request.ClientRequestDTO;
+import com.github.guiziin227.cozidosrestaurant.dto.response.ClientResponseDTO;
+import com.github.guiziin227.cozidosrestaurant.mapper.ClientMapper;
 import com.github.guiziin227.cozidosrestaurant.model.Client;
 import com.github.guiziin227.cozidosrestaurant.service.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,30 +25,37 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @Autowired
+    private ClientMapper clientMapper;
+
     @PostMapping
-    public ResponseEntity<Client> save(@RequestBody Client client) {
-        Client savedClient = clientService.create(client);
-        return ResponseEntity.ok(savedClient);
+    public ResponseEntity<ClientResponseDTO> save(@RequestBody @Valid ClientRequestDTO client) {
+        Client toEntity = clientMapper.toEntity(client);
+        Client savedClient = clientService.create(toEntity);
+        ClientResponseDTO responseDTO = clientMapper.toResponseDTO(savedClient);
+        return ResponseEntity.ok(responseDTO);
     }
 
     @PutMapping("/{cpf}")
-    public ResponseEntity<Client> update(@PathVariable String cpf,@RequestBody Client client) {
-        Client updatedClient = clientService.update(cpf,client);
-        return ResponseEntity.ok(updatedClient);
+    public ResponseEntity<ClientResponseDTO> update(@PathVariable String cpf,@RequestBody ClientRequestDTO client) {
+        Client toEntity = clientMapper.toEntity(client);
+        Client updatedClient = clientService.update(cpf, toEntity);
+        ClientResponseDTO responseDTO = clientMapper.toResponseDTO(updatedClient);
+        return ResponseEntity.ok(responseDTO);
     }
 
     @GetMapping("/{cpf}")
-    public ResponseEntity<Client> getByCpf(@PathVariable String cpf) {
+    public ResponseEntity<ClientResponseDTO> getByCpf(@PathVariable String cpf) {
         Client client = clientService.findByCpf(cpf);
         if (client == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(client);
+        return ResponseEntity.ok(clientMapper.toResponseDTO(client));
     }
 
     @GetMapping
-    public ResponseEntity<List<Client>> getAll() {
+    public ResponseEntity<List<ClientResponseDTO>> getAll() {
         List<Client> clients = clientService.findAll();
-        return ResponseEntity.ok().body(clients);
+        return ResponseEntity.ok().body(clientMapper.toResponseDTOList(clients));
     }
 }
