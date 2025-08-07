2f866897c67ca57f6597fba240a7d89880d415a2
Guiziin227 <gh63388@gmail.com>
2025-08-07 09:59:49 -0300

    Add methods to check existence and retrieve client by CPF in ClientRepository

--- src/main/java/com/github/guiziin227/cozidosrestaurant/repository/ClientRepository.java
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/repository/ClientRepository.java
@@ -4,8 +4,12 @@ import com.github.guiziin227.cozidosrestaurant.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+    boolean existsByCpf(String cpf);
+
+    Optional<Client> findByCpf(String cpf);
 }
