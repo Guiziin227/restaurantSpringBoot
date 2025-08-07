8159f1e569db02518d104d9a4199d64921b9f9a1
Guiziin227 <gh63388@gmail.com>
2025-08-07 14:10:38 -0300

    Add JSON date format annotations to ClientResponseDTO fields

--- src/main/java/com/github/guiziin227/cozidosrestaurant/dto/response/ClientResponseDTO.java
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/dto/response/ClientResponseDTO.java
@@ -1,5 +1,7 @@
 package com.github.guiziin227.cozidosrestaurant.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -8,7 +10,9 @@ public record ClientResponseDTO(
         String name,
         String cpf,
         int age,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime createdAt,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime updatedAt
 ) {
 }
