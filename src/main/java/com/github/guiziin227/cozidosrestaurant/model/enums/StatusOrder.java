b24d725020cba1ef89b100db0a93f241185b8733
Guiziin227 <gh63388@gmail.com>
2025-08-07 14:18:33 -0300

    Add entity classes for Tables, Waiter, Order, and Status enums

---
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/model/enums/StatusOrder.java
@@ -0,0 +1,9 @@
+package com.github.guiziin227.cozidosrestaurant.model.enums;
+
+public enum StatusOrder {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED,
+    FINISHED,
+}
---
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/model/enums/StatusTable.java
@@ -0,0 +1,7 @@
+package com.github.guiziin227.cozidosrestaurant.model.enums;
+
+public enum StatusTable {
+    AVAILABLE,
+    OCCUPIED,
+    RESERVED,
+}
