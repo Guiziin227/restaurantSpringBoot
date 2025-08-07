41596adf4a54e415c4bfec88c61af835e34c6dc2
Guiziin227 <gh63388@gmail.com>
2025-08-07 09:59:35 -0300

    Implement for custom exceptions

---
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/exceptions/handler/CustomHandlerException.java
@@ -0,0 +1,42 @@
+package com.github.guiziin227.cozidosrestaurant.exceptions.handler;
+
+import com.github.guiziin227.cozidosrestaurant.exceptions.ExceptionResponse;
+import com.github.guiziin227.cozidosrestaurant.exceptions.UserExistsException;
+import com.github.guiziin227.cozidosrestaurant.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+public class CustomHandlerException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({UserNotFoundException.class})
+    public final ResponseEntity<ExceptionResponse> handleNotFoundException(Exception ex, WebRequest request) {
+        ExceptionResponse resp = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(resp, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({UserExistsException.class})
+    public final ResponseEntity<ExceptionResponse> handleExistsException(Exception ex, WebRequest request) {
+        ExceptionResponse resp = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(resp, HttpStatus.CONFLICT);
+    }
+
+
+
+}
