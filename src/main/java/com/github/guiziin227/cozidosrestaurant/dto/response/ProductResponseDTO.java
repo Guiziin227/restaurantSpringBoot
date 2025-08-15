12268564b82ce62f6b400f2a620a56c0d3b7dda1
Guiziin227 <gh63388@gmail.com>
2025-08-15 12:25:42 -0300

    Add createdAt and updatedAt fields to ProductResponseDTO with JSON formatting

--- src/main/java/com/github/guiziin227/cozidosrestaurant/dto/response/ProductResponseDTO.java
+++ src/main/java/com/github/guiziin227/cozidosrestaurant/dto/response/ProductResponseDTO.java
@@ -1,10 +1,20 @@
 package com.github.guiziin227.cozidosrestaurant.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
 public record ProductResponseDTO(
         Long id,
         String name,
         String description,
         Double price,
-        Boolean prepared
+        Boolean prepared,
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt,
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime updatedAt
 ) {
 }
